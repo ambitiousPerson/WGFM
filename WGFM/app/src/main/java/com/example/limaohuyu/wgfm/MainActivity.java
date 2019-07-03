10a5ad03e1119c8e3a82da4509cc965b3c6ffbe5
崔志伟 <czwaizbq@163.com>
2019-07-03 18:40:24 +0800

    小试牛刀

--- WGFM/app/src/main/java/com/example/limaohuyu/wgfm/MainActivity.java
+++ WGFM/app/src/main/java/com/example/limaohuyu/wgfm/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.limaohuyu.wgfm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvLane1;
+    private TextView mTvLane2;
+    private TextView mTvLane3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("我的项目");
+        initView();
+
+
+    }
+
+
+    private void initView() {
+        mTvLane1 = (TextView) findViewById(R.id.tv_lane1);
+        mTvLane1.setText("我的打的");
+        mTvLane2 = (TextView) findViewById(R.id.tv_lane2);
+        mTvLane2.setText("我的打的1");
+        mTvLane3 = (TextView) findViewById(R.id.tv_lane3);
+        mTvLane3.setText("我的打的2");
+
+
     }
 }
